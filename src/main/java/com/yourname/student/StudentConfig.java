7b2ac6f683848786e277a46aa23f9107ae37e44b
User <sh.alyyew2019@gmail.com>
2021-09-08 18:51:25 +0500

    elishi first commit

---
+++ src/main/java/com/yourname/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.yourname.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository studentRepository) {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam@gmail.com",
+                    LocalDate.of(2000, Month.NOVEMBER, 11)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, Month.MARCH, 18)
+            );
+
+            studentRepository.saveAll(
+                    List.of(mariam,alex)
+            );
+
+        };
+    }
+
+}
